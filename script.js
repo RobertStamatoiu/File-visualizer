598e934c7c23c9bedbb5764da8c6f0011f82df65
Robert <robertstamatoiu284@gmail.com>
2026-08-09 15:14:46 +0300

    configured the terminal mostly

--- script.js
+++ script.js
@@ -4,12 +4,19 @@ const RowSeparator = document.getElementById("horizontal-separator");
 const ColSeparator = document.getElementById("vertical-separator");
 
 
+const Panel = Object.freeze({
+    FILE: "file",
+    TERMINAL: "terminal",
+    TREE: "tree"
+});
+
 let row_dragging = false;
 let col_dragging = false;
 let min_height = 200;
 let min_width = 230;
-
-
+let focused = Panel.TERMINAL;
+let command = "";
+let current_dir = "";
 
 RowSeparator.addEventListener('mousedown', () => { row_dragging = true; });
 ColSeparator.addEventListener('mousedown', () => { col_dragging = true; });
@@ -26,4 +33,14 @@ document.addEventListener('mousemove', (event) => {
         const y = event.clientY;
         layout.style.gridTemplateRows = `${Math.max(min_height, Math.min(y, window.innerHeight - min_height))}px 5px 1fr`;
     }
+})
+
+document.addEventListener('keydown', (event) => {
+    let key = event.key;
+    if (key.length === 1) {
+        command += key;
+    } else if (key === "Backspace"){
+        command = command.slice(0, -1);
+    }
+    document.getElementById("command").textContent = current_dir + "> " + command;
 })
